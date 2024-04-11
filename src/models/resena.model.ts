3eda76feda0115062edf9ebb874a3e9629ae865c
Luis Henao García <luis.henao37085@ucaldas.edu.co>
2024-04-11 13:44:27 -0500

    Creación de entidades Pago, MetodoPago, MetodoPagoCliente, Reseña y Factura

---
+++ src/models/resena.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Resena extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  idResena?: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaResena: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  calificacion: number;
+
+  @property({
+    type: 'string',
+  })
+  comentario?: string;
+
+
+  constructor(data?: Partial<Resena>) {
+    super(data);
+  }
+}
+
+export interface ResenaRelations {
+  // describe navigational properties here
+}
+
+export type ResenaWithRelations = Resena & ResenaRelations;
