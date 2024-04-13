7de1cc9293798b491af59019c31191cbeec02196
Luis Henao García <luis.henao37085@ucaldas.edu.co>
2024-04-13 18:01:26 -0500

    Ajuste exportar modelos desde index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,9 +2,17 @@ export * from './beneficiario.model';
 export * from './ciudad.model';
 export * from './cliente-plan.model';
 export * from './cliente.model';
+export * from './departamento.model';
 export * from './estado-beneficiario.model';
+export * from './factura.model';
+export * from './metodo-pago-cliente.model';
+export * from './metodo-pago.model';
 export * from './pago.model';
 export * from './plan-servicio-plan.model';
 export * from './plan.model';
-export * from './servicio-plan.model';
+export * from './resena.model';
+export * from './sala.model';
+export * from './servicio-funerario.model';
+export * from './servicio-plan.model'
+export * from './sede.model';
 
