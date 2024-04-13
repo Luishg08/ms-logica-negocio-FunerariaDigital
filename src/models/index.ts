adb82fa02d7e4c915c4866edadf6a5ac799cf10a
Luis Henao García <luis.henao37085@ucaldas.edu.co>
2024-04-13 17:56:54 -0500

    Ajustes

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,10 @@
 export * from './beneficiario.model';
+export * from './ciudad.model';
+export * from './cliente-plan.model';
 export * from './cliente.model';
 export * from './estado-beneficiario.model';
 export * from './pago.model';
-export * from './servicio-plan.model';
 export * from './plan-servicio-plan.model';
-export * from './cliente-plan.model';
+export * from './plan.model';
+export * from './servicio-plan.model';
+
