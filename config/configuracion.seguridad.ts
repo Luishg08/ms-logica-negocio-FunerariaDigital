0f7f8ea175fbac4037abd086802ccd15f082e230
sophiCardona <sophia.cardona39931@ucaldas.edu.co>
2024-04-17 16:55:22 -0500

    configuracion Seguridad

---
+++ config/configuracion.seguridad.ts
@@ -0,0 +1,8 @@
+export namespace ConfiguracionSeguridad {
+export const menuUsuarioId = "63d04fcd013402040c040968";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
+}
