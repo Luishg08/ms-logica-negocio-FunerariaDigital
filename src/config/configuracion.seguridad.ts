0b0e86466dfe736109ee6775053d29e7aef593bd
Luis Henao García <luis.henao37085@ucaldas.edu.co>
2024-04-19 22:29:47 -0500

    Implementando algunas estrategias

--- src/config/configuracion.seguridad.ts
+++ src/config/configuracion.seguridad.ts
@@ -1,6 +1,8 @@
 export namespace ConfiguracionSeguridad {
   export const menuBeneficiarioId = "661e84c8d778277dd4744632";
   export const menuClienteId = "66205541de146c9100e29468";
+  export const menuFacturaId = "66212a29ba9e7667e83a489d"
+  export const menuCiudadId = "662057a6bc9988c7830c5136"
   export const listarAccion = "listar";
   export const guardarAccion = "guardar";
   export const editarAccion = "editar";
